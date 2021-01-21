ffeef44454e7f7d117df86a92f5a961496121aa1
achyuth reddy <ncsachyuth@gmail.com>
2021-01-21 23:27:13 +0530

    created create and read option on the comment

--- functions/handlers/geeks.js
+++ functions/handlers/geeks.js
@@ -41,3 +41,67 @@ exports.createOneGeek = (req, res) => {
       console.log(err)
     })
 }
+
+exports.getGeekById = (req, res) => {
+  let geekData = {}
+  db.doc(`/geek/${req.params.geekId}`)
+    .get()
+    .then(doc => {
+      if (!doc.exists) {
+        return res.status(404).json({ error: 'geek not found' })
+      } else {
+        geekData = doc.data()
+        geekData.geekId = doc.id
+        return db
+          .collection('comments')
+          .orderBy('createdAt', 'desc')
+          .where('geekId', '==', req.params.geekId)
+          .get()
+      }
+    })
+    .then(data => {
+      geekData.comments = []
+      data.forEach(doc => {
+        geekData.comments.push(doc.data())
+      })
+      return res.status(200).json(geekData)
+    })
+    .catch(err => {
+      console.error(err)
+      res.status(500).json({ error: err })
+    })
+}
+
+exports.deleteGeekById = () => {}
+
+exports.commentOnGeek = (req, res) => {
+  if (req.body.body.trim() === '') {
+    return res.status(404).json({ error: 'comment should not be empty' })
+  }
+  const newComment = {
+    body: req.body.body,
+    createdAt: new Date().toISOString(),
+    geekId: req.params.geekId,
+    userHandle: req.user.handle,
+    userImage: req.user.imageUrl
+  }
+  // FIXME: get the latest image of the user or create a new query to the database
+
+  db.doc(`/geek/${req.params.geekId}`)
+    .get()
+    .then(doc => {
+      if (!doc.exists) {
+        return res.status(400).json({ error: 'this geek does not exist' })
+      } else {
+        return db.collection('comments').add(newComment)
+      }
+    })
+    .then(doc => {
+      res.json({ message: `comment with ${doc.id} created successfully` })
+    })
+    .catch(err => {
+      console.error(err)
+      res.status(500).json({ error: 'something went wrong' })
+    })
+}
+// exports.
--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,11 @@
 const functions = require('firebase-functions')
-const { getAllGeeks, createOneGeek } = require('./handlers/geeks')
+const {
+  getAllGeeks,
+  createOneGeek,
+  getGeekById,
+  deleteGeekById,
+  commentOnGeek
+} = require('./handlers/geeks')
 const {
   signUp,
   login,
@@ -21,6 +27,13 @@ app.get('/user/', FBAuth, getUserDetails)
 // geeks routes
 
 app.get('/geeks', getAllGeeks)
-app.post('/geeks', FBAuth, createOneGeek)
+app.post('/geek', FBAuth, createOneGeek)
+// RD operations on the geeks
+app.get('/geek/:geekId', getGeekById)
+app.delete('/geek/:geekId', deleteGeekById)
+// Like and unlike the geeks
+app.get('/geek/:geekId', getGeekById)
+// comment on a geek
+app.post('/geek/:geekId/comment', FBAuth, commentOnGeek)
 
 exports.api = functions.https.onRequest(app)
